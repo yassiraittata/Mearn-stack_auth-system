e86853e9702abd8af0bf0ef9e39b707d7e244dc5
Yassir aittata <yassir.aittata@gmail.com>
2025-08-06 19:27:35 +0100

    auth middleware

---
+++ server/middleware/userAuth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import env from "../utils/validateEnv.js";
+import createError from "http-errors";
+
+const userAuth = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return next(
+      createError(401, "Not Authorised. Make sure you are logged in!")
+    );
+  }
+
+  const decoded = jwt.verify(token, env.JWT_SECRET);
+
+  if (!decoded || !decoded.id) {
+    return next(
+      createError(401, "Not Authorised. Make sure you are logged in!")
+    );
+  }
+
+  req.userId = decoded.id;
+  next();
+};
+
+export default userAuth;
