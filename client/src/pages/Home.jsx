3509d37fdf851705ffc9453266f82569c752f137
Yassir aittata <yassir.aittata@gmail.com>
2025-08-12 18:15:10 +0100

    add bg

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 function Home() {
   return (
     <>
-      <div>
+      <div className="flex flex-col items-center justify-center max-h-screen bg-[url('/bg_img.png')] bg-cover bg-center">
         <NavBar />
         <Header />
       </div>
